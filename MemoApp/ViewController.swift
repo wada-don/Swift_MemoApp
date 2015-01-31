ec5aff93b884dd085bb9ffd8d7ae1bd7331d1625
wadadon <wadadon@dawanoMacBook-Pro.local>
2015-01-31 17:43:36 +0900

    first commit

--- MemoApp/ViewController.swift
+++ MemoApp/ViewController.swift
@@ -8,16 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var tableview : UITableView!
+    var texts = ["hello", "world", "hello", "Swift"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableview.delegate = self
+        tableview.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return texts.count
+    }
+    
+   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
+     let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+    //
+    cell.textLabel?.text = texts[indexPath.row]
+    return cell
     }
 
 
