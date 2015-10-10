ab3a4338d25b2ee57b5fae489f7e7bbc08a0b915
wada-don <rw.ryu.18.wr@gmail.com>
2015-10-10 15:05:54 +0900

    Parseエラー2

---
+++ MemoApp/Supporting Files/MemoApp-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef MemoApp_MemoApp_Bridging_Header_h
+#define MemoApp_MemoApp_Bridging_Header_h
+
+#import <Parse/Parse.h>
+
+#endif
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'MemoApp' do
+pod 'Parse', '~> 1.8.5’
+end
+
+target 'MemoAppTests' do
+
+end
+
